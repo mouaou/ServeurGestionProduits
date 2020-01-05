7820338ba5071cea1c486fb68edf33869c3cf8c7
ymouaou <youssef.mouaou@gmail.com>
2020-01-05 19:57:24 +0100

    first commit

---
+++ src/test/java/com/org/produits/ServeurGestionProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
